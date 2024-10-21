1b488bb2fc748a3f0043a5f43590030ce790a2a5
solagent99 <davidanderson9469@gmail.com>
2024-10-21 16:41:55 -0700

    fix: print unused error variables to console

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -49,10 +49,11 @@ export const updateSession = async (request: NextRequest) => {
     }
 
     return response;
-  } catch (e) {
+  } catch (error) {
     // If you are here, a Supabase client could not be created!
     // This is likely because you have not set up environment variables.
     // Check out http://localhost:3000 for Next Steps.
+    console.error(error);
     return NextResponse.next({
       request: {
         headers: request.headers,
