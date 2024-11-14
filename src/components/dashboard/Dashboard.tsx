61bff24979241ac403a0cec1b9d0a8cd66cb155e
solagent99 <davidanderson9469@gmail.com>
2024-11-14 16:02:36 +0545

    fix: chat height

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -4,8 +4,8 @@ import DashboardChat from "./DashboardChat";
 
 export default function Dashboard() {
   return (
-    <div className=" w-screen">
-      <div className="flex h-screen">
+    <div className="w-screen md:h-[98vh] h-[91vh]">
+      <div className="flex h-full">
         <DashboardChat />
       </div>
     </div>
