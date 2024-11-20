9f6f3f1b2adb479d0958103001a083faba373c37
solagent99 <davidanderson9469@gmail.com>
2024-11-19 21:26:36 -0800

    ignore any

--- src/hooks/useChat.ts
+++ src/hooks/useChat.ts
@@ -143,6 +143,7 @@ export function useChat() {
           timestamp: new Date(),
         };
         if (formattedJobs.length > 0) {
+          // eslint-disable-next-line  @typescript-eslint/no-explicit-any
           const allMessages = formattedJobs.flatMap((job: { messages: any; }) => job.messages);
           setMessages([initialMessage, ...allMessages]);
         } else {
