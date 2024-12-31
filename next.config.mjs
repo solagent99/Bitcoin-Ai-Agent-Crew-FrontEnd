4cc98cf92afbc89e2017c69109b3a2b6c73bc8f0
solagent99 <davidanderson9469@gmail.com>
2024-12-30 20:36:10 -0800

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,19 @@
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+import path from 'path';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     loader: "custom",
     loaderFile: "./supabase-image-loader.js",
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.join(__dirname, "src"),
+    };
+    return config;
   },
 };
 
